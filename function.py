46523fd6f491287605393746a95e6e5a2c979f73
satorugojo1223 <swastikrajawat48@gmail.com>
2026-01-20 17:46:45 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,5 @@
+def well_wish():
+    print("well wish")
+    print("how are you")
+
+well_wish()
---
+++ weathercondition.py
@@ -0,0 +1,6 @@
+def weather_condition():
+    print(spring)
+    print(autumn)
+spring= "the weather is really hot near here have a cola "
+autumn = "the weather is cold dang lemme put on smth"
+weather_condition()
